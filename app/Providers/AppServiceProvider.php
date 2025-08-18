1b48264c7e081b04834321d54dc9a30d44d28c19
K <mallariking0@gmail.com>
2025-08-18 11:52:50 +0800

    17.2 pdocution fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Force HTTPS URLs when behind proxy (like Cloudflare)
+        if (config('app.env') === 'production' || env('FORCE_HTTPS', false)) {
+            URL::forceScheme('https');
+        }
+
+        // Trust proxies (for Cloudflare)
+        // if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        //     request()->setTrustedProxies(['*'], \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL);
+        // }
     }
 }
